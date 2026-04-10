195f5e8a15c9e97e7bc7f5172708232c17397d70
BANKAI@369 <sameersam3a@gmail.com>
2026-04-10 16:50:16 +0530

    updated the ORM mappings

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -22,16 +22,21 @@ export class User extends BaseEntity {
 
   @ManyToOne(() => Department, (department) => department.employees, {
     nullable: true,
+    onDelete: "SET NULL",
   })
+  @JoinColumn({ name: "department_id" })
   department: Department | null;
 
-  @Column({ type: "varchar", nullable: true })
+  @Column({ name: "department_id", type: "uuid", nullable: true })
   departmentId: string | null;
 
-  @ManyToOne(() => Role, (role) => role.users, { nullable: true })
-  @JoinColumn({ name: "roleId" })
+  @ManyToOne(() => Role, (role) => role.users, {
+    nullable: true,
+    onDelete: "SET NULL",
+  })
+  @JoinColumn({ name: "role_id" })
   role: Role | null;
 
-  @Column({ type: "varchar", nullable: true })
+  @Column({ name: "role_id", type: "uuid", nullable: true })
   roleId: string | null;
 }
